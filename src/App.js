6aa7c8428fd416d10f813b6c353fe221c69439cb
PiNdoye94 <PiNdoye94>
2021-11-06 15:09:28 +0000

    tuto react sur l'integration d'un editeur de texte

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import TextEditor from './components/TextEditor';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Text Editor</h1>
       </header>
+      <div className="editor">
+        <TextEditor />
+      </div>
+      <footer className="footer">
+        By <a href="http://Teknuflex.tech">TeknuFlex</a> Editor used is <a href="https://jpuri.github.io/react-draft-wysiwyg/#/">React Draft Wysiwyg</a>
+      </footer>
     </div>
   );
 }
